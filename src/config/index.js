96180643ec121fbf3f66b3fa7bd64741cae75159
Lory Jiang <funtree9@icloud.com>
2019-10-14 23:13:25 +0800

    add config.audit

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,11 @@
 const config = require('./config')
 const configProd = require('./config_prod')
 const configTest = require('./config_test')
+const configAudit = require('./config_audit')
 const configDev = require('./config_dev')
 const configLocal = require('./config_local')
 const merge = require('deepmerge')
-const NodeEnv = {Prod: 'prod', Test: 'test', Dev: 'dev', Local: 'local'}
+const NodeEnv = {Prod: 'prod', Test: 'test', Dev: 'dev', Local: 'local', Audit: 'audit'}
 
 const env = NodeEnv.Local
 
@@ -12,13 +13,14 @@ const diffEnvAction = {
   [NodeEnv.Local]: configLocal,
   [NodeEnv.Test]: configTest,
   [NodeEnv.Development]: configDev,
-  [NodeEnv.Production]: configProd
+  [NodeEnv.Production]: configProd,
+  [NodeEnv.Audit]: configAudit
 }
 
 const usedConfig = diffEnvAction[env]
 if (!usedConfig) throw new Error(`config file for env ${env} is not found`)
 
-module.exports = (
+export default (
   /**
    * @return {config}
    */
